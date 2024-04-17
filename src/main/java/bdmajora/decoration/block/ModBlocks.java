061439c242643d1135f2b403c1f8676ad22d8ffc
bdmajora <bdmajora@protonmail.com>
2024-04-16 21:08:18 -0600

    Added treadmills

--- src/main/java/bdmajora/decoration/block/ModBlocks.java
+++ src/main/java/bdmajora/decoration/block/ModBlocks.java
@@ -2,7 +2,9 @@ package bdmajora.decoration.block;
 
 import bdmajora.decoration.block.dragonfly.BlockModel;
 import bdmajora.decoration.block.gym.BlockGymLocker;
+import bdmajora.decoration.block.gym.BlockTreadMill;
 import bdmajora.decoration.block.metastates.gym.GymLockerMetaState;
+import bdmajora.decoration.block.metastates.gym.TreadMillMetaState;
 import net.minecraft.client.render.block.color.BlockColorWater;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.material.Material;
@@ -44,6 +46,11 @@ public class ModBlocks {
 		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/gym/carpet2.json"),
 			getOrCreateBlockState(MOD_ID, "gym/carpet2.json"), null, true))
 		.build(new BlockModel("carpet2", UtilIdRegistrar.nextIdBlock(), Material.metal, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/gym/carpet2.json")));
+
+	public static final Block treadmill = new BlockBuilder(MOD_ID)
+		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/gym/treadmill.json"),
+			getOrCreateBlockState(MOD_ID, "gym/treadmill.json"), new TreadMillMetaState(), true))
+		.build(new BlockTreadMill("treadmill", UtilIdRegistrar.nextIdBlock(), Material.metal, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/gym/treadmill.json")));
 
 	//arcade
 	public static final Block creeperPlushy = new BlockBuilder(MOD_ID)
