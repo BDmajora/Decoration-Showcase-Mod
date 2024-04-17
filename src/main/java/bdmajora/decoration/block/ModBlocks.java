071d1f603b08dd4aa3116b9d6461e56640519aab
bdmajora <bdmajora@protonmail.com>
2024-04-16 18:11:35 -0600

    Gym craft added

--- src/main/java/bdmajora/decoration/block/ModBlocks.java
+++ src/main/java/bdmajora/decoration/block/ModBlocks.java
@@ -1,14 +1,14 @@
 package bdmajora.decoration.block;
 
+import bdmajora.decoration.block.dragonfly.BlockModel;
+import bdmajora.decoration.block.gym.BlockGymLocker;
+import bdmajora.decoration.block.metastates.gym.GymLockerMetaState;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.material.Material;
-import net.minecraft.core.block.tag.BlockTags;
-import net.minecraft.core.sound.BlockSounds;
 import turniplabs.halplibe.helper.BlockBuilder;
 import bdmajora.decoration.UtilIdRegistrar;
-import bdmajora.decoration.block.dragonfly.BlockBlender;
-import bdmajora.decoration.block.dragonfly.BlockModel;
-import bdmajora.decoration.block.metastates.BlenderMetaState;
+import bdmajora.decoration.block.mcf.BlockBlender;
+import bdmajora.decoration.block.metastates.mcf.BlenderMetaState;
 import useless.dragonfly.helper.ModelHelper;
 import useless.dragonfly.model.block.BlockModelDragonFly;
 
@@ -16,10 +16,22 @@ import static bdmajora.decoration.decoration.MOD_ID;
 
 public class ModBlocks {
 
+	//mcf
 	public static final Block blender = new BlockBuilder(MOD_ID)
-		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/blender_pre.json"),
-			ModelHelper.getOrCreateBlockState(MOD_ID, "blender.json"), new BlenderMetaState(), true))
-		.build(new BlockBlender("blender", UtilIdRegistrar.nextIdBlock(), Material.metal, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/blender_pre.json")));
+		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/mcf/blender_pre.json"),
+			ModelHelper.getOrCreateBlockState(MOD_ID, "mcf/blender.json"), new BlenderMetaState(), true))
+		.build(new BlockBlender("blender", UtilIdRegistrar.nextIdBlock(), Material.metal, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/mcf/blender_pre.json")));
+
+	//gym
+	public static final Block gymLocker = new BlockBuilder(MOD_ID)
+		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/gym/gymlocker.json"),
+			ModelHelper.getOrCreateBlockState(MOD_ID, "gym/gymlocker.json"), new GymLockerMetaState(), true))
+		.build(new BlockGymLocker("gymLocker", UtilIdRegistrar.nextIdBlock(), Material.metal, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/gym/gymlocker.json")));
+
+	public static final Block carpet1 = new BlockBuilder(MOD_ID)
+		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/gym/carpet1.json"),
+			ModelHelper.getOrCreateBlockState(MOD_ID, "gym/carpet1.json"), null, true))
+		.build(new BlockModel("carpet1", UtilIdRegistrar.nextIdBlock(), Material.metal, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/gym/carpet1.json")));
 
 	public static void register() {
 	}
