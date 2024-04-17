1e548e30d1f78660956182c018fb05828893bdc4
bdmajora <bdmajora@protonmail.com>
2024-04-17 02:41:56 -0600

    More gym models

--- src/main/java/bdmajora/decoration/block/ModBlocks.java
+++ src/main/java/bdmajora/decoration/block/ModBlocks.java
@@ -1,10 +1,8 @@
 package bdmajora.decoration.block;
 
 import bdmajora.decoration.block.dragonfly.BlockModel;
-import bdmajora.decoration.block.gym.BlockGymLocker;
-import bdmajora.decoration.block.gym.BlockTreadMill;
-import bdmajora.decoration.block.metastates.gym.GymLockerMetaState;
-import bdmajora.decoration.block.metastates.gym.TreadMillMetaState;
+import bdmajora.decoration.block.gym.*;
+import bdmajora.decoration.block.metastates.gym.*;
 import net.minecraft.client.render.block.color.BlockColorWater;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.material.Material;
@@ -52,14 +50,38 @@ public class ModBlocks {
 			getOrCreateBlockState(MOD_ID, "gym/treadmill.json"), new TreadMillMetaState(), true))
 		.build(new BlockTreadMill("treadmill", UtilIdRegistrar.nextIdBlock(), Material.metal, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/gym/treadmill.json")));
 
+	public static final Block yogamat = new BlockBuilder(MOD_ID)
+		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/gym/yogamat.json"),
+			getOrCreateBlockState(MOD_ID, "gym/yogamat.json"), new YogamatMetaState(), true))
+		.build(new BlockYogamat("yogamat", UtilIdRegistrar.nextIdBlock(), Material.metal, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/gym/yogamat.json")));
+
+	public static final Block aerobicStepper = new BlockBuilder(MOD_ID)
+		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/gym/aerobicstepper.json"),
+			getOrCreateBlockState(MOD_ID, "gym/aerobicstepper.json"), new AerobicStepperMetaState(), true))
+		.build(new BlockAerobicStepper("aerobicStepper", UtilIdRegistrar.nextIdBlock(), Material.metal, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/gym/aerobicstepper.json")));
+
+	public static final Block benchgc = new BlockBuilder(MOD_ID)
+		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/gym/benchgc.json"),
+			getOrCreateBlockState(MOD_ID, "gym/benchgc.json"), new BenchgcMetaState(), true))
+		.build(new BlockBenchgc("benchgc", UtilIdRegistrar.nextIdBlock(), Material.metal, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/gym/benchgc.json")));
+
+	public static final Block brickWallGC = new BlockBuilder(MOD_ID)
+		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/gym/brickwallgc.json")))
+		.build(new BlockModel("brickwallgc", UtilIdRegistrar.nextIdBlock(), Material.metal, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/gym/brickwallgc.json")));
+
+	public static final Block crossTrainer = new BlockBuilder(MOD_ID)
+		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/gym/crosstrainer.json"),
+			getOrCreateBlockState(MOD_ID, "gym/crosstrainer.json"), new CrossTrainerMetaState(), true))
+		.build(new BlockBenchgc("crossTrainer", UtilIdRegistrar.nextIdBlock(), Material.metal, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/gym/crosstrainer.json")));
+
 	//arcade
 	public static final Block creeperPlushy = new BlockBuilder(MOD_ID)
 		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/arcade/creeperplushie.json")))
-		.build(new BlockModel("creeperPlushy", UtilIdRegistrar.nextIdBlock(), Material.metal, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/arcade/creeperplushie.json")));
+		.build(new BlockModel("creeperPlushy", UtilIdRegistrar.nextIdBlock(), Material.cloth, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/arcade/creeperplushie.json")));
 
 	public static final Block pigPlushy = new BlockBuilder(MOD_ID)
 		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/arcade/pigplushie.json")))
-		.build(new BlockModel("pigPlushy", UtilIdRegistrar.nextIdBlock(), Material.metal, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/arcade/pigplushie.json")));
+		.build(new BlockModel("pigPlushy", UtilIdRegistrar.nextIdBlock(), Material.cloth, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/arcade/pigplushie.json")));
 
 
 	public static void register() {
